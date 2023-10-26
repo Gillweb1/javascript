0c619f65c17a61c0d38530978b60191a2dd095ab
Gillweb1 <146372108+Gillweb1@users.noreply.github.com>
2023-10-26 17:45:42 +0000

    data types conversion

---
+++ basics/data.js
@@ -0,0 +1,10 @@
+let score = "33";
+
+//  console.log(typeof score);
+
+let numberInt = Number(score);
+// console.log(typeof numberInt);
+
+let boolians = 1;
+let boolianchng = Boolean(boolians)
+console.log(boolianchng);
